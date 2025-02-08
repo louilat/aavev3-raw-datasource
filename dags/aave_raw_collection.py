1446e0dcb4d5b9f6817be082b6157cb2c5993378
louilat <lmm.latournerie@gmail.com>
2025-02-08 11:04:17 +0100

    update dag

--- dags/aave_raw_collection.py
+++ dags/aave_raw_collection.py
@@ -5,17 +5,35 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 )
 
 with DAG(
-    dag_id="example_kubernetes_pod",
+    dag_id="aavev3-raw-collection",
     schedule="@once",
     start_date=datetime(2025, 3, 30),
 ) as dag:
-    example_kpo = KubernetesPodOperator(
+    events_collector = KubernetesPodOperator(
         kubernetes_conn_id="k8s_conn",
-        image="hello-world",
-        name="airflow-test-pod",
+        image="louilat/aavev3-raw-events-collector",
+        name="raw_events_collection",
         task_id="task-one",
         is_delete_operator_pod=True,
         get_logs=True,
     )
 
-    example_kpo
+    events_decoder = KubernetesPodOperator(
+        kubernetes_conn_id="k8s_conn",
+        image="louilat/aavev3-raw-events-decoder",
+        name="raw_events_decoding",
+        task_id="task-two",
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
+
+    balances_collector = KubernetesPodOperator(
+        kubernetes_conn_id="k8s_conn",
+        image="louilat/aavev3-raw-balances-collector",
+        name="raw_balances_collection",
+        task_id="task-two",
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
+
+    events_collector >> events_decoder >> balances_collector
